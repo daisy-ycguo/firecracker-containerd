8e789d28aacef5b04105dd1c879fed709211454d
Yingchun Guo <yingchun.guo@intel.com>
2023-08-22 18:24:18 +0800

    fix small error in debugging tool

--- tools/main/run.sh
+++ tools/main/run.sh
@@ -1,6 +1,6 @@
 pushd $(pwd)
 cd /var/lib/firecracker-containerd/shim-base
-sed -i "s/default#.*\//$(ls default#* -d)\//g" /home/xinranwa/go/src/github.com/firecracker-microvm/firecracker-containerd/tools/main/ttrpc_client.go
+sed -i "s/default#.*\//$(ls default#* -d)\//g" /home/ubuntu/go/src/github.com/firecracker-microvm/firecracker-containerd/tools/main/ttrpc_client.go
 popd
 
 go run ttrpc_client.go
